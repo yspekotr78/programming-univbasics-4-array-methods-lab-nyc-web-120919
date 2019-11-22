a6c0e6dc1235e265ebf6a574367277844d2fb763
Yury Spektor <yspektor78@gmail.com>
2019-11-22 21:42:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,15 +7,15 @@ def using_sort(array)
 end
 
 def using_reverse(array)
-
+  return array.reverse
 end
 
 def using_first(array)
-
+  return array.first
 end
 
 def using_last(array)
-
+  return array.last
 end
 
 def using_size(array)
